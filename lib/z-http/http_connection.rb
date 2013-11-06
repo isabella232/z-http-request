d51f3d08b894d7bbc3fe5957336cef01f43c6f1c
Benedikt Böhm <bb@xnull.de>
2013-11-06 16:41:56 +0100

    revert timeout fix

--- lib/z-http/http_connection.rb
+++ lib/z-http/http_connection.rb
@@ -166,6 +166,7 @@ module ZMachine
     end
 
     def unbind(reason = nil)
+      reason ||= Errno::ETIMEDOUT if @conn.channel.timedout?
       @clients.map { |c| c.unbind(reason) }
 
       if r = @pending.shift
